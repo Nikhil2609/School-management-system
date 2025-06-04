6a69c7bfd7249f0933642f7194c11e7e971bd6d2
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2025-06-04 13:36:15 +0530

    remove category modal

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import AuthRepository from '../repository/auth.repository';
 import AuthService from '../service/auth.service';
 import AuthController from '../controller/auth.controller';
-import rateLimiter from 'express-rate-limit';
 
 const authRouter = express.Router();
 
@@ -10,15 +9,7 @@ const authRepository = new AuthRepository();
 const authService = new AuthService(authRepository);
 const authController = new AuthController(authService);
 
-const loginLimiter = rateLimiter({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 5, // limit each IP to 5 request
-  message: {
-    error: 'Too many login attempts from this IP, please try again later'
-  }
-});
-
-authRouter.post('/login', loginLimiter, authController.login);
+authRouter.post('/login', authController.login);
 authRouter.post('/register', authController.register);
 
 authRouter.post('/users', authController.getThirdPartyUserList);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import categoryRouter from './category.routes';
 import authRouter from './auth.routes';
 import { authorizeToken } from '../middleware/auth.middleware';
 import productRouter from './product.routes';
@@ -7,8 +6,6 @@ import productRouter from './product.routes';
 const router = express.Router();
 
 router.use('/auth', authRouter);
-// router.use('/categories', authorizeToken, categoryRouter);
-router.use('/categories', authorizeToken, categoryRouter);
 router.use('/products', authorizeToken, productRouter);
 
 export default router;
